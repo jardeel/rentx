e3cc0423c4ebc89ab0a3d33bdcf1fd490a7ef780
Jardel Sousa <jardeelsousa@hotmail.com>
2022-08-03 05:10:58 -0300

    Create car component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,7 +5,29 @@ import { RFValue} from 'react-native-responsive-fontsize';
 import Logo from '../../assets/logo.svg';
 import { Container, Header, TotalCars, HeaderContent } from './styles';
 
+import { Car }  from '../../components/Car';
+
 export function Home(){
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  }
+
+  const carDataTwo = {
+    brand: 'porsche ',
+    name: 'Panamera ',
+    rent: {
+      period: 'AO DIA',
+      price: 340,
+    },
+    thumbnail: 'https://i.pinimg.com/originals/69/54/c2/6954c273d9f969214fb3777c4b995f63.png'
+  }
+
   return (
     <Container>
       <StatusBar 
@@ -21,6 +43,8 @@ export function Home(){
           </TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={carDataOne}/>
+      <Car data={carDataTwo}/>
     </Container>
   )
 }
