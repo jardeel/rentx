5a2ac35c52f56c33f76c95a521a52cc34d2d2e88
Jardel Sousa <jardeelsousa@hotmail.com>
2022-08-13 06:36:05 -0300

    Created Components for CarDetails and Styling

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,19 @@
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
-import { Container, Header, CarImages } from './styles';
+import { 
+  Container, 
+  Header, 
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
+} from './styles';
 
 export function CarDetails(){
   return (
@@ -14,6 +27,27 @@ export function CarDetails(){
           imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Audi</Brand>
+            <Name>Audi A3</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+          Vitae vel similique iusto numquam. Voluptatibus molestiae 
+          quod quisquam debitis atque! Voluptatibus sequi in ullam 
+          blanditiis ut.
+        </About>
+      </Content>
     </Container>
   )
 }
