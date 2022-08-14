95d82872eaa0cf5756273e2ce379790a2860caaf
Jardel Sousa <jardeelsousa@hotmail.com>
2022-08-14 07:16:09 -0300

    Created Acessory Component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,5 +1,14 @@
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
+
 import { 
   Container, 
   Header, 
@@ -12,7 +21,8 @@ import {
   Rent,
   Period,
   Price,
-  About
+  About,
+  Acessories
 } from './styles';
 
 export function CarDetails(){
@@ -40,6 +50,15 @@ export function CarDetails(){
             <Price>R$ 580</Price>
           </Rent>
         </Details>
+
+        <Acessories>
+          <Acessory name="380Km/h" icon={speedSvg}/>
+          <Acessory name="3.2s" icon={accelerationSvg}/>
+          <Acessory name="800 HP" icon={forceSvg}/>
+          <Acessory name="Gasoline" icon={gasolineSvg}/>
+          <Acessory name="Auto" icon={exchangeSvg}/>
+          <Acessory name="2 pessoas" icon={peopleSvg}/>
+        </Acessories>
 
         <About>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
